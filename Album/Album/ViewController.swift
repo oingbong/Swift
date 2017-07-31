a0b6eb338db0cf9c9091a3c16729f647c8b430f0
oingbong <oingbong@gmail.com>
2017-07-31 22:58:50 +0900

    Album git init

---
+++ Album/Album/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Album
+//
+//  Created by Dkbmc Developer on 2017. 7. 31..
+//  Copyright © 2017년 Oingbong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
