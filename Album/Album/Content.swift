af46eb4957a46f6e2e316ab561b1f238f29f7d7d
oingbong <oingbong@gmail.com>
2017-08-03 08:50:26 +0900

    make variable for paging

--- Album/Album/Content.swift
+++ Album/Album/Content.swift
@@ -5,10 +5,16 @@ class Content: UIViewController {
     // MARK : - Variable
     @IBOutlet weak var ContentImageView: UIImageView!
     @IBOutlet weak var ContentLabel: UILabel!
+    
+    var PageIndex = Int()
+    var ContentImage = String()
+    var ContentText = String()
     // Variable_End
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        ContentImageView.image = UIImage(named: ContentImage)
+        ContentLabel.text = ContentText
     }
-
-
 }
