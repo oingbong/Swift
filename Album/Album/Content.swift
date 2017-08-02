7dc808cd703fcea7b48a90f23a378ad29e5dab82
oingbong <oingbong@gmail.com>
2017-08-03 08:46:09 +0900

    Make Label and ImageView Connect Stoyboard and swift file

---
+++ Album/Album/Content.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class Content: UIViewController {
+
+    // MARK : - Variable
+    @IBOutlet weak var ContentImageView: UIImageView!
+    @IBOutlet weak var ContentLabel: UILabel!
+    // Variable_End
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
