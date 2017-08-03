391fcda5ba181a0b25359e7b1f3f67cd9c53791a
oingbong <oingbong@gmail.com>
2017-08-04 08:51:24 +0900

    Add Detail photo Define InitialView and dataSource

--- Album/Album/DashBoard.swift
+++ Album/Album/DashBoard.swift
@@ -19,19 +19,29 @@ class DashBoard: UIViewController, UIPageViewControllerDataSource {
         
 
         /*
+            0-1. 처음 화면 정의(initialView 를 Content에 있는 것으로 사용합니다, 형태는 VC)
+            0-2. Array 형식으로 viewVC 정의
             1. storyBaord 에 있는 PageViewVC 라는 id 가진 것을 가져옵니다.
             2. PageViewVC 를 PageView 에 크기를 맞추겠다는 의미
             3. PageViewVC 를 PageView 에 추가시키겠다는 의미
             4. 하위에 있는 ViewController를 등록하기
             5. View : 그냥 보여주기 / ViewController : 여러 기능 동작
                 하위(PageViewController)에 있는 VC의 명령어 및 기능까지 상위(PageView)로 다 가져오겠다는 의미
+            6. dataSource 외부가 아닌 밑에 만들어놓은 것에서 가져옵니다.
+            7. 가지고 있는 VC 를 등록합니다. 0-2 정의한 것 등록합니다.
          */
+        
+        // Layout //
+        let InitialView = ContentVCIndex(index: 0) as Content // 0-1
+        let ViewController = NSArray(object: InitialView) //0-2
         
         let PageVC = self.storyboard?.instantiateViewController(withIdentifier: "PageVC") as! UIPageViewController // 1
         PageVC.view.frame = PageView.bounds // 2
         PageView.addSubview(PageVC.view) // 3
         addChildViewController(PageVC) // 4
         PageVC.didMove(toParentViewController: self) // 5
+        PageVC.dataSource = self // 6
+        PageVC.setViewControllers(ViewController as? [UIViewController], direction: .forward, animated: true, completion: nil) // 7
     }
 
 // MARK : - Page View
