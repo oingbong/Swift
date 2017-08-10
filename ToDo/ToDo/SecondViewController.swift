905e49fca76bec6157647339226b3f78de12da43
oingbong <oingbong@gmail.com>
2017-08-11 08:45:14 +0900

    Todo init and using tabbed application

---
+++ ToDo/ToDo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ToDo
+//
+//  Created by Dkbmc Developer on 2017. 8. 11..
+//  Copyright © 2017년 Oingbong. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
