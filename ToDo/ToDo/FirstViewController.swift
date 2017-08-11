abc6d606f864bf78a152fcefb795da9d95a9c0fa
oingbong <oingbong@gmail.com>
2017-08-11 09:30:24 +0900

    Made tableView and tableViewCell Connect datasource and delegate

--- ToDo/ToDo/FirstViewController.swift
+++ ToDo/ToDo/FirstViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var list = ["Buy milk", "Run 5 mils", "Get Peter", "Plant my new plants"]
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(list.count)
+        return (list.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return (cell)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
