45205273129cc025f05c40031f8abc92385a26aa
oingbong <oingbong@gmail.com>
2017-08-13 02:19:03 +0900

    make NSObject

---
+++ NewsReader/NewsReader/Article.swift
@@ -0,0 +1,18 @@
+//
+//  Article.swift
+//  NewsReader
+//
+//  Created by Dkbmc Developer on 2017. 8. 13..
+//  Copyright © 2017년 Oingbong. All rights reserved.
+//
+
+import UIKit
+
+class Article: NSObject {
+
+    var headline: String?
+    var desc: String?
+    var author: String?
+    var url: String?
+    var imageUrl: String?
+}
