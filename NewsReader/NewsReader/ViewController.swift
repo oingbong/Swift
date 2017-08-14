3a74c9de3e06d4edc8cee87d25d2536c6728ddd2
oingbong <oingbong@gmail.com>
2017-08-14 14:55:23 +0900

    Connect url and make json

--- NewsReader/NewsReader/ViewController.swift
+++ NewsReader/NewsReader/ViewController.swift
@@ -12,13 +12,51 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var tableView: UITableView!
     
+    var articles: [Article]? = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchArticle()
+    }
+    
+    func fetchArticle(){
+        let urlRequest = URLRequest(url: URL(string:"https://newsapi.org/v1/articles?source=techcrunch&sortBy=top&apiKey=7eb2a61f124944b78b316ab112134a3c")!)
+        
+        let task = URLSession.shared.dataTask(with: urlRequest){ (data, response, error) in
+            
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            self.articles = [Article]()
+            do{
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: AnyObject]
+                
+                if let articlesFromJson = json["articles"] as? [[String: AnyObject]] {
+                    for articleFromJson in articlesFromJson {
+                        let article = Article()
+                        if let title = articleFromJson["title"] as? String, let author = articleFromJson["author"] as? String, let desc = articleFromJson["description"] as? String, let url = articleFromJson["url"] as? String, let urlToImage = articleFromJson["urlToImage"] as? String {
+                            
+                            article.author = author
+                            article.desc = desc
+                            article.headline = title
+                            article.url = url
+                            article.imageUrl = urlToImage
+                        }
+                        self.articles?.append(article)
+                    }
+                }
+            } catch let error {
+                print(error)
+            }
+            
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "articleCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "articleCell", for: indexPath) as! ArticleCell
+        cell.title.text = "This is Test"
         return cell
     }
     
