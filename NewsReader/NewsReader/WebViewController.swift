f00296ab81b439f2330443a5e0e58751b17eaa25
oingbong <oingbong@gmail.com>
2017-08-15 22:52:36 +0900

    Make WebView UI and Connect storyboard and viewController(WebView)

---
+++ NewsReader/NewsReader/WebViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WebViewController.swift
+//  NewsReader
+//
+//  Created by Dkbmc Developer on 2017. 8. 15..
+//  Copyright © 2017년 Oingbong. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
