a0f00bc3b19a125acff9d97be12eac9f1149b144
oingbong <oingbong@gmail.com>
2017-08-19 18:51:07 +0900

    Add Menu Tap and add Method about that

---
+++ NewsReader/NewsReader/BaseViewCell.swift
@@ -0,0 +1,24 @@
+ //
+//  BaseViewCell.swift
+//  NewsReader
+//
+//  Created by Dkbmc Developer on 2017. 8. 19..
+//  Copyright © 2017년 Oingbong. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- NewsReader/NewsReader/MenuManager.swift
+++ NewsReader/NewsReader/MenuManager.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class MenuManager: NSObject {
+class MenuManager: NSObject, UITableViewDelegate, UITableViewDataSource {
     let blackView = UIView()
+    let menuTableView = UITableView()
+    let arrayOfSources = ["TechCrunch","TechRadar"]
     
     public func openMenu(){
         if let window = UIApplication.shared.keyWindow {
             blackView.frame = window.frame
             blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
             
+            blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.dismissMenu)))
+            
+            let height : CGFloat = 100
+            let y = window.frame.height - height
+            
+            menuTableView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
+            
             window.addSubview(blackView)
+            window.addSubview(menuTableView)
             
             UIView.animate(withDuration: 0.5, animations: {
                 self.blackView.alpha = 1
+                self.menuTableView.frame.origin.y = y
             })
         }
     }
@@ -27,7 +38,39 @@ class MenuManager: NSObject {
     public func dismissMenu(){
         UIView.animate(withDuration: 0.5, animations: {
             self.blackView.alpha = 0
+            if let window = UIApplication.shared.keyWindow{
+                self.menuTableView.frame.origin.y = window.frame.height
+            }
         })
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfSources.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = arrayOfSources[indexPath.item]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    override init(){
+        super.init()
+        menuTableView.delegate = self
+        menuTableView.dataSource = self
+        
+        menuTableView.isScrollEnabled = false
+        menuTableView.bounces = false
+        
+        menuTableView.register(BaseViewCell.classForCoder(), forCellReuseIdentifier: "cellId")
     }
     
 }
