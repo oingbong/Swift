4dd6bc5b266d65e9e3e39f456c13bcaaccc545b4
oingbong <oingbong@gmail.com>
2017-08-27 14:48:43 +0900

    [Swift-Tabbed App] git init

---
+++ CoinDeal/CoinDeal/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CoinDeal
+//
+//  Created by Dkbmc Developer on 2017. 8. 27..
+//  Copyright © 2017년 oingbong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
