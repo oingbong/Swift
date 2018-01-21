a049c08a9a6210befde19144eed5c12e62a44516
oingbong <oingbong@gmail.com>
2018-01-21 17:37:35 +0900

    Reduce code of IBAction

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -11,35 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var resultDisplay: UILabel!
     
-    @IBAction func btn0(_ sender: UIButton) {
-        resultDisplay.text = resultDisplay.text! + "0"
-    }
-    @IBAction func btn1(_ sender: UIButton) {
-        resultDisplay.text = resultDisplay.text! + "1"
-    }
-    @IBAction func btn2(_ sender: UIButton) {
-        resultDisplay.text = resultDisplay.text! + "2"
-    }
-    @IBAction func btn3(_ sender: UIButton) {
-        resultDisplay.text = resultDisplay.text! + "3"
-    }
-    @IBAction func btn4(_ sender: UIButton) {
-        resultDisplay.text = resultDisplay.text! + "4"
-    }
-    @IBAction func btn5(_ sender: UIButton) {
-        resultDisplay.text = resultDisplay.text! + "5"
-    }
-    @IBAction func btn6(_ sender: UIButton) {
-        resultDisplay.text = resultDisplay.text! + "6"
-    }
-    @IBAction func btn7(_ sender: UIButton) {
-        resultDisplay.text = resultDisplay.text! + "7"
-    }
-    @IBAction func btn8(_ sender: UIButton) {
-        resultDisplay.text = resultDisplay.text! + "8"
-    }
-    @IBAction func btn9(_ sender: UIButton) {
-        resultDisplay.text = resultDisplay.text! + "9"
+    @IBAction func btnNumber(btnN : UIButton){
+        let button = btnN.titleLabel?.text
+        resultDisplay.text = resultDisplay.text! + button!
     }
     
     
