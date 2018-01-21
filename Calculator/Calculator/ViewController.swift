2d0580bfcf049d5844a5e75390c83fa88b3590a9
oingbong <oingbong@gmail.com>
2018-01-21 17:13:54 +0900

    Update Number IBAction

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,15 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resultDisplay: UILabel!
+    
+    @IBAction func btn0(_ sender: UIButton) {
+        resultDisplay.text = resultDisplay.text! + "0"
+    }
+    @IBAction func btn1(_ sender: UIButton) {
+        resultDisplay.text = resultDisplay.text! + "1"
+    }
+    @IBAction func btn2(_ sender: UIButton) {
+        resultDisplay.text = resultDisplay.text! + "2"
+    }
+    @IBAction func btn3(_ sender: UIButton) {
+        resultDisplay.text = resultDisplay.text! + "3"
+    }
+    @IBAction func btn4(_ sender: UIButton) {
+        resultDisplay.text = resultDisplay.text! + "4"
+    }
+    @IBAction func btn5(_ sender: UIButton) {
+        resultDisplay.text = resultDisplay.text! + "5"
+    }
+    @IBAction func btn6(_ sender: UIButton) {
+        resultDisplay.text = resultDisplay.text! + "6"
+    }
+    @IBAction func btn7(_ sender: UIButton) {
+        resultDisplay.text = resultDisplay.text! + "7"
+    }
+    @IBAction func btn8(_ sender: UIButton) {
+        resultDisplay.text = resultDisplay.text! + "8"
+    }
+    @IBAction func btn9(_ sender: UIButton) {
+        resultDisplay.text = resultDisplay.text! + "9"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
